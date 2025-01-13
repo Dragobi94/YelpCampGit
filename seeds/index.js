a10d11e2cdb73fb2167fa40ea330a54ab1a86d9b
Dragobi94 <132261771+Dragobi94@users.noreply.github.com.>
2025-01-13 15:07:17 +0100

    retrack#

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const dbUrl = process.env.DB_URL
 
 mongoose.set('strictQuery', true);
 mongoose.connect(dbUrl);
+//mongoose.connect('mongodb+srv://cosentinojacopo:rKyhU8Cvon0NAKjq@cluster0.slnv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 //mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
 const db = mongoose.connection;
